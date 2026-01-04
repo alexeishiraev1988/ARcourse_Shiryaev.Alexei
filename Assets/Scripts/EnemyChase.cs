3ab1d13b1534666ef5ed2cc01d68e1b3c7288794
Alexey <alexeishiraev@mail.ru>
2026-01-04 13:36:30 +0300

    NavMesh

---
+++ Assets/Scripts/EnemyChase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyChase : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerClickMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClickMove : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
